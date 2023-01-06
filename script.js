c7c479d2f822684f83760f7bdea7c8f85a75b7dc
Asherbeck10 <asher.bec@gmail.com>
2023-01-06 09:10:21 +0000

    creating password option and random it is a test version

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Array of special characters to be included in password
-var specialCharacters = [
+let specialCharacters = [
   '@',
   '%',
   '+',
@@ -26,10 +26,10 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+let lowerCasedCharacters = [
   'a',
   'b',
   'c',
@@ -59,7 +59,7 @@ var lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
+let upperCasedCharacters = [
   'A',
   'B',
   'C',
@@ -90,26 +90,45 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  let numberCharacters = prompt("Number of characters,Please enter a number between 10 and 64.");
+  console.log(numberCharacters)
+  let userLowercase=confirm("Wold you like to use lowercase characters?\nIf yes press ok if no press cancel\nRemember you should have at least one type of character")
+  console.log(userLowercase)
+  let userUppercase=confirm("Wold you like to use uppercase characters?\nIf yes press ok if no press cancel\nRemember you should have at least one type of character")
+  console.log(userUppercase)
+  let userNumeric=confirm("Wold you like to use numeric characters?\nIf yes press ok if no press cancel\nRemember you should have at least one type of character")
+  console.log(userNumeric)
+  let userSpecialCharacters=confirm("Wold you like to use special characters characters?\nIf yes press ok if no press cancel\nRemember you should have at least one type of character")
+  console.log(userSpecialCharacters)
+  return
 
+
+
+  
+  
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+  randomCharacter=arr[Math.floor((Math.random()*arr.length))]
+  console.log( "random" +randomCharacter)
 
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  getPasswordOptions()
+  getRandom(numericCharacters)
 
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+let generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  let password = generatePassword();
+  let passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 }
